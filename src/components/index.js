94f74e09fe25f15f6fac6cde2fec2fef91a266d3
Lewis Hardie <lewishardie@gmail.com>
2024-02-21 12:37:09 +1100

    update: auth page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Sidebar } from './Sidebar';
 export { default as UserProfile } from './UserProfile';
-export { default as LoginForm } from './LoginForm'
 export { default as Auth } from './Auth'
+export { default as Navbar } from './Navbar'
