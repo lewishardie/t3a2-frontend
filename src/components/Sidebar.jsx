d86e4fe637c2cbf8113adee829838a17768aeb76
Lewis Hardie <lewishardie@gmail.com>
2024-02-22 17:57:20 +1100

    blocker: tailwind doesn't like me

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to ='/' style={({isActive}) => isActive ? {color: "red"} : undefined }>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to ='/explore' style={({isActive}) => isActive ? {color: "red"} : undefined }>Explore</NavLink>
+          </li>
+          <li>
+            <NavLink to ='/chats' style={({isActive}) => isActive ? {color: "red"} : undefined }>Chats</NavLink>
+          </li>
+          <li>
+            <NavLink to ='/friends' style={({isActive}) => isActive ? {color: "red"} : undefined }>Friends</NavLink>
+          </li>
+          <li>
+            <NavLink to ='/notifications' style={({isActive}) => isActive ? {color: "red"} : undefined }>Notifications</NavLink>
+          </li>
+          <li>
+            <NavLink to ='/settings' style={({isActive}) => isActive ? {color: "red"} : undefined }>Settings</NavLink>
+          </li>
+        </ul>
+        <div>Sidebar</div>
+      </nav>
+    </header>
   )
 }
 
