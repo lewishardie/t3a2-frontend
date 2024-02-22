ea3e7ba596bc46d8bf060a8cd3be100e2aaf7382
Lewis Hardie <lewishardie@gmail.com>
2024-02-22 17:09:46 +1100

    update: messing around with context/provider for user

--- src/components/auth/Forms/Register.jsx
+++ src/components/auth/Forms/Register.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import { useState } from 'react'
+import { useBackend } from '../../../context/BackendProvider'
 
 export const Register = (props) => {
+    // variables for user registration form
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const registerUser = (e) => {
+    // Access backend-related functions
+    const { postData } = useBackend();
+
+    // prevent reload
+    const registerUser = async (e) => {
         e.preventDefault();
-        console.log(username)
-    }
+        console.log(name, username, email, password)
+
+        try {
+            const userData = {
+                name,
+                username,
+                email,
+                password
+            }
+
+            await postData(userData);
+
+        } catch (error) {
+            console.error('Faied to register user:', error)
+        };
+    };
 
     return (
         <div>
-            <form className="login-form" onSubmit={registerUser}>
-                <label for="name">Name</label>
-                <input type="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-                <label for="username">Username</label>
-                <input type="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <label for="email">Email</label>
+            <form className="register-form" onSubmit={registerUser}>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label htmlFor="email">Email</label>
                 <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <label for="password">Password</label>
-                <input type="password" id="username" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Log In</button>
             </form>
         </div>
