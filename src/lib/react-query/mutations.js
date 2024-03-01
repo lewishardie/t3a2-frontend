482176be9a84badb5312b214ecc55de37840bb69
Lewis Hardie <lewishardie@gmail.com>
2024-03-01 20:22:55 +1100

    fix: getting rid on unused vars

--- src/lib/react-query/mutations.js
+++ src/lib/react-query/mutations.js
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { createUserAccount } from '../api/api';
 
 export const useCreateUserAccount = () => {
